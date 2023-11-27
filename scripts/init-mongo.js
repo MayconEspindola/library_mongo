5e663b247fc7cff4ee6066a3f664690f22a7aa2e
Maycon Espindola <mycone10@gmail.com>
2023-11-27 13:37:45 -0300

    Atualização Biblioteca Akashic

--- scripts/init-mongo.js
+++
@@ -1,8 +0,0 @@
-db.createUser({
-    user: "bibliotecario",
-    pwd: "library-mongo21112023-master",
-    roles: [
-        { role: "readWrite", db: "Biblioteca" },
-        { role: "readWrite", db: "Biblioteca", collection: "" }
-    ]
-});
--- work/app/config/database.php
+++ work/app/config/database.php
@@ -2,36 +2,33 @@
 
 namespace app\config;
 
-require_once __DIR__ . '/../../vendor/autoload.php';
-
 use MongoDB\Client;
-use MongoDB\BSON\ObjectId;
+use MongoDB\Driver\Exception\Exception as MongoDBException;
+use MongoDB\BSON\Regex;
+
+require_once __DIR__ . '/../../vendor/autoload.php';
+require_once 'EnvironmentSettings.php';
 
 class Database
 {
     public static function getConnection()
     {
-        $envpath = realpath(dirname(__FILE__) . '/../env.ini');
-        $env = parse_ini_file($envpath);
-
-        $uri = sprintf(
-            'mongodb://%s:%s@%s:%s',
-            $env['username'],
-            $env['password'],
-            $env['host'],
-            $env['porta'],
-            $env['database']
-        );
-
         try {
+            $envSettings = new EnvironmentSettings();
+            $env = $envSettings->obterConfiguracoes();
+
+            $uri = sprintf(
+                'mongodb://%s:%s@%s:%s',
+                $env['DATABASE']['username'],
+                $env['DATABASE']['password'],
+                $env['DATABASE']['host'],
+                $env['DATABASE']['porta']
+            );
+
             $client = new Client($uri);
-            return $client->selectDatabase($env['database']);
-        } catch (\MongoDB\Driver\Exception\ConnectionTimeoutException $e) {
-            die("Erro de conexão: Tempo de conexão expirado. Verifique as configurações do servidor MongoDB.");
-        } catch (\MongoDB\Driver\Exception\AuthenticationException $e) {
-            die("Erro de autenticação: As credenciais fornecidas são inválidas. Verifique o nome de usuário e a senha.");
-        } catch (\MongoDB\Driver\Exception\Exception $e) {
-            die("Erro ao conectar ao MongoDB: " . $e->getMessage());
+            return $client->selectDatabase($env['DATABASE']['database']);
+        } catch (MongoDBException $e) {
+            throw new \RuntimeException("Erro ao conectar ao MongoDB: " . $e->getMessage());
         }
     }
 
@@ -48,8 +45,8 @@ class Database
         try {
             $result = $collection->find($filter);
             return $result;
-        } catch (\MongoDB\Driver\Exception\Exception $e) {
-            die("Erro na consulta: " . $e->getMessage());
+        } catch (MongoDBException $e) {
+            throw new \RuntimeException("Erro na consulta: " . $e->getMessage());
         }
     }
 
@@ -66,30 +63,34 @@ class Database
         try {
             $collection->insertOne($document);
             return true;
-        } catch (\MongoDB\Driver\Exception\BulkWriteException $e) {
-            die("Erro ao inserir documento: " . $e->getMessage());
-        } catch (\MongoDB\Driver\Exception\Exception $e) {
-            die("Erro ao inserir documento: " . $e->getMessage());
+        } catch (MongoDBException $e) {
+            throw new \RuntimeException("Erro ao inserir documento: " . $e->getMessage());
         }
     }
 
     public static function deleteDocument($collectionName, $filter)
     {
         $database = self::getConnection();
-
+    
         if ($database === null) {
             return false;
         }
-
+    
         $collection = $database->selectCollection($collectionName);
-
+    
         try {
-            $collection->deleteOne($filter);
-            return true;
-        } catch (\MongoDB\Driver\Exception\Exception $e) {
-            die("Erro ao excluir documento: " . $e->getMessage());
+            $result = $collection->deleteOne($filter);
+    
+            if ($result->getDeletedCount() > 0) {
+                return true;
+            } else {
+                return false;
+            }
+        } catch (MongoDBException $e) {
+            throw new \RuntimeException("Erro ao excluir documento: " . $e->getMessage());
         }
     }
+    
 
     public static function updateDocument($collectionName, $filter, $update)
     {
@@ -104,8 +105,8 @@ class Database
         try {
             $collection->updateOne($filter, $update);
             return true;
-        } catch (\MongoDB\Driver\Exception\Exception $e) {
-            die("Erro ao atualizar documento: " . $e->getMessage());
+        } catch (MongoDBException $e) {
+            throw new \RuntimeException("Erro ao atualizar documento: " . $e->getMessage());
         }
     }
 }
